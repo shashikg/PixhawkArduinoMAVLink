0b90f626bac744e30564a508e18ebf45f0aa7d18
shashikg <shashikg@iitk.ac.in>
2018-06-23 21:38:33 +0800

    Added functionality to read acceleration data.

---
+++ libraries/src/PixhawkArduinoMAVLink.cpp
@@ -0,0 +1,41 @@
+/*
+  PixhawkArduinoMAVLink.cpp - Library for using Arduino to recieve Pixhawk sensors data.
+  Created by Shashi Kant, June 23, 2018.
+  Using MAVLink C headers files for the ardupilotmega one.
+*/
+
+#include "ardupilotmega/mavlink.h"
+#include "PixhawkArduinoMAVLink.h"
+#include <SoftwareSerial.h>
+
+PixhawkArduinoMAVLink::PixhawkArduinoMAVLink(int RX_PIN, int TX_PIN){
+  _MAVSerial = SoftwareSerial(RX_PIN, TX_PIN);
+  _RX = RX_PIN;
+  _TX = TX_PIN;
+}
+
+bool PixhawkArduinoMAVLink::begin(){
+  _MAVSerial.begin(57600);
+  return _MAVSerial;
+}
+
+void PixhawkArduinoMAVLink::ReadAcceleration(float *xacc, float *yacc, float *zacc, bool *success){
+  mavlink_message_t msg;
+  mavlink_status_t status;
+
+  if(_MAVSerial.available()){
+    uint8_t ch = _MAVSerial.read();
+    if(mavlink_parse_char(MAVLINK_COMM_0, ch, &msg, &status)){
+      switch(msg.msgid){
+        case MAVLINK_MSG_ID_HIL_SENSOR:
+        {
+          mavlink_hil_sensor_t imu;
+          mavlink_msg_hil_sensor_decode(&msg, &imu);
+          xacc = imu.xacc;
+          yacc = imu.yacc;
+          zacc = imu.zacc;
+        }break;
+      }
+    }
+  }
+}
--- libraries/src/PixhawkArduinoMAVLink.h
+++ libraries/src/PixhawkArduinoMAVLink.h
@@ -1,3 +1,25 @@
-// Main header file to be used in Arduino Code this basically just include the mavlink.h from the ardupilotmega directory.
+/*
+  PixhawkArduinoMAVLink.h - Library for using Arduino to recieve Pixhawk sensors data.
+  Created by Shashi Kant, June 23, 2018.
+  Using MAVLink C headers files for the ardupilotmega one.
+*/
+
+#ifndef PixhawkArduinoMAVLink_h
+#define PixhawkArduinoMAVLink_h
 
 #include "ardupilotmega/mavlink.h"
+#include <SoftwareSerial.h>
+
+class PixhawkArduinoMAVLink
+{
+  public:
+    PixhawkArduinoMAVLink(int RX_PIN, int TX_PIN);
+    bool begin();
+    void ReadAcceleration(float *xacc, float *yacc, float *zacc, bool *success);
+  private:
+    int _RX;
+    int _TX;
+    SoftwareSerial _MAVSerial;
+};
+
+#endif
